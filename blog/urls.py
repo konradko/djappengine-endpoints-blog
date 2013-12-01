3cdc9b65f22f0331c3a55c490c1bd752c0753b51
konradko <konrad.korzel@gmail.com>
2013-12-01 19:32:18 +0000

    Refactor urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,11 @@
 from django.conf.urls import patterns, url
 from django.conf import settings
 
-from blog import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.home_page, name='home_page'),
-    url(r'^new$', views.new_article, name='new_article'),
-    url(r'^edit/([-\w]+)/$', views.edit_article, name='edit_article'),
-    url(r'^delete/([-\w]+)/$', views.delete_article, name='delete_article'),
+urlpatterns = patterns('blog.views',
+    url(r'^$', 'home_page', name='home_page'),
+    url(r'^new$', 'new_article', name='new_article'),
+    url(r'^edit/([-\w]+)/$', 'edit_article', name='edit_article'),
+    url(r'^delete/([-\w]+)/$', 'delete_article', name='delete_article'),
 )
 
 if settings.DEBUG:
