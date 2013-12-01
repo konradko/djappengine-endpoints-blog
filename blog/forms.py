f336e02f0ee870a79a4e29f9e07bd2d82bc41148
konradko <konrad.korzel@gmail.com>
2013-12-01 21:51:54 +0000

    Add placeholder for title, change validation error text, make delete not required

--- blog/forms.py
+++ blog/forms.py
@@ -7,6 +7,9 @@ class BaseArticleForm(forms.Form):
     title = forms.CharField(
         required=True,
         error_messages={'required': "Title can't be empty"},
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter title',
+        })
     )
 
     content = forms.CharField(
@@ -23,8 +26,8 @@ class NewArticleForm(BaseArticleForm):
         data = self.cleaned_data['title']
         # Check that title is unique, needed for slug uniqueness
         if Article.query(Article.title == data).get():
-            raise forms.ValidationError("You already have article with that title!")
+            raise forms.ValidationError("You already have article with that title")
         return data
 
 class EditArticleForm(BaseArticleForm):
-    delete = forms.BooleanField()
+    delete = forms.BooleanField(required=False)
