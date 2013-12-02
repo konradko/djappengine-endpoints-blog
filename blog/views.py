a781691a73373fadcaea465d56d20b497e968911
konradko <konrad.korzel@gmail.com>
2013-12-02 00:24:34 +0000

    Add function generating unique slugs, remove del article view

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,32 @@
 from django.shortcuts import render, redirect
 from django.template.defaultfilters import slugify
-from django.http import Http404
 from datetime import datetime
 
-from blog.forms import EditArticleForm, NewArticleForm
+from blog.forms import EditArticleForm, ArticleForm
 from blog.models import Article
 
+# Helper functions
 def get_article(article_slug):
-    article = Article.query(Article.slug == article_slug).get()
-    if not article:
-        raise Http404
-    else:
-        return article
+    return Article.query(Article.slug == article_slug).get()
 
+def get_unique_slug(title):
+    article_slug = slugify(title)[:79]
+    if get_article(article_slug):
+        original_slug = article_slug
+        counter = 2
+        while get_article(article_slug):
+            article_slug = "%s-%i" % (original_slug, counter)
+            counter += 1
+    return article_slug
+
+# Views
 def new_article(request):
-    form = NewArticleForm(request.POST or None)
+    form = ArticleForm(request.POST or None)
     if form.is_valid():
         article = Article(
             title=form.cleaned_data['title'],
             content=form.cleaned_data['content'],
-            slug=slugify(form.cleaned_data['title'])[:79]
+            slug=get_unique_slug(form.cleaned_data['title'])
         )
         article.put()
         return redirect('/')
@@ -30,19 +37,15 @@ def edit_article(request, article_slug):
     form = EditArticleForm(request.POST or None, initial=article.to_dict())
     if form.is_valid():
         if form.cleaned_data['delete']:
-            get_article(article_slug).key.delete()
+            article.key.delete()
         else:
             article.title = form.cleaned_data['title']
             article.content = form.cleaned_data['content']
-            article.slug = slugify(form.cleaned_data['title'])[:79]
+            article.slug = get_unique_slug(form.cleaned_data['title'])
             article.last_update = datetime.now()
             article.put()
         return redirect('/')
     return render(request, 'article.html', {'form': form})
-
-def delete_article(request, article_slug):
-    get_article(article_slug).key.delete()
-    return redirect('/')
 
 def home_page(request):
     articles = Article.query().order(-Article.created).fetch()
