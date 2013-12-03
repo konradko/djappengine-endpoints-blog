9f4dd89b601684eda8563e332862e19c5b9504ad
konradko <konrad.korzel@gmail.com>
2013-12-03 22:54:39 +0000

    Change view helper functions to model classmethods

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from google.appengine.ext import ndb
+from django.template.defaultfilters import slugify
 
 class Article(ndb.Model):
     title = ndb.StringProperty(required=True)
@@ -9,6 +10,35 @@ class Article(ndb.Model):
 
     def __unicode__(self):
         return self.title
+
+    @classmethod
+    def find(self, article_slug):
+        return self.query(self.slug == article_slug).get()
+
+    @classmethod
+    def get_new(self, title, content):
+        article = self(
+            title=title,
+            content=content,
+        )
+        return article.save().get()
+
+    @classmethod
+    def get_unique_slug(self, title):
+        article_slug = slugify(title)[:79]
+        if not article_slug:
+            article_slug = 's'
+        if self.find(article_slug):
+            original_slug = article_slug
+            counter = 2
+            while self.find(article_slug):
+                article_slug = "%s-%i" % (original_slug, counter)
+                counter += 1
+        return article_slug
+
+    def save(self):
+        self.slug = self.get_unique_slug(self.title)
+        return self.put()
 
     def get_absolute_url(self):
         return "/edit/%s" % self.slug
--- blog/views.py
+++ blog/views.py
@@ -1,39 +1,13 @@
 from django.shortcuts import render, redirect
-from django.template.defaultfilters import slugify
 from datetime import datetime
 
 from blog.forms import EditArticleForm, ArticleForm
 from blog.models import Article
 
-# Helper functions
-def find_article(article_slug):
-    return Article.query(Article.slug == article_slug).get()
-
-def get_unique_slug(title):
-    article_slug = slugify(title)[:79]
-    if not article_slug:
-        article_slug = 's'
-    if find_article(article_slug):
-        original_slug = article_slug
-        counter = 2
-        while find_article(article_slug):
-            article_slug = "%s-%i" % (original_slug, counter)
-            counter += 1
-    return article_slug
-
-def get_new_article(title, content):
-    article = Article(
-        title=title,
-        content=content,
-        slug=get_unique_slug(title)
-    )
-    return article.put().get()
-
-# Views
 def new_article(request):
     form = ArticleForm(request.POST or None)
     if form.is_valid():
-        get_new_article(
+        Article.get_new(
             title=form.cleaned_data['title'],
             content=form.cleaned_data['content'],
         )
@@ -41,7 +15,7 @@ def new_article(request):
     return render(request, 'article.html', {'form': form})
 
 def edit_article(request, article_slug):
-    article = find_article(article_slug)
+    article = Article.find(article_slug)
     form = EditArticleForm(request.POST or None, initial=article.to_dict())
     if request.POST.get('delete'):
         article.key.delete()
@@ -50,8 +24,7 @@ def edit_article(request, article_slug):
         article.title = form.cleaned_data['title']
         article.content = form.cleaned_data['content']
         article.last_update = datetime.now()
-        article.slug = get_unique_slug(article.title)
-        article.put()
+        article.save()
         return redirect('/')
     return render(request, 'article.html', {'form': form})
 
