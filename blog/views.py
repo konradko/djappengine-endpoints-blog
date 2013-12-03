87d07c1ddc7e1dd74be058a5ddfaa02fcee604b6
konradko <konrad.korzel@gmail.com>
2013-12-03 00:37:14 +0000

    Add new article function, rename get_article function, fix edit_article to delete without valid form

--- blog/views.py
+++ blog/views.py
@@ -6,43 +6,51 @@ from blog.forms import EditArticleForm, ArticleForm
 from blog.models import Article
 
 # Helper functions
-def get_article(article_slug):
+def find_article(article_slug):
     return Article.query(Article.slug == article_slug).get()
 
 def get_unique_slug(title):
     article_slug = slugify(title)[:79]
-    if get_article(article_slug):
+    if not article_slug:
+        article_slug = 's'
+    if find_article(article_slug):
         original_slug = article_slug
         counter = 2
-        while get_article(article_slug):
+        while find_article(article_slug):
             article_slug = "%s-%i" % (original_slug, counter)
             counter += 1
     return article_slug
+
+def get_new_article(title, content):
+    article = Article(
+        title=title,
+        content=content,
+        slug=get_unique_slug(title)
+    )
+    return article.put().get()
 
 # Views
 def new_article(request):
     form = ArticleForm(request.POST or None)
     if form.is_valid():
-        article = Article(
+        get_new_article(
             title=form.cleaned_data['title'],
             content=form.cleaned_data['content'],
-            slug=get_unique_slug(form.cleaned_data['title'])
         )
-        article.put()
         return redirect('/')
     return render(request, 'article.html', {'form': form})
 
 def edit_article(request, article_slug):
-    article = get_article(article_slug)
+    article = find_article(article_slug)
     form = EditArticleForm(request.POST or None, initial=article.to_dict())
+    if request.POST.get('delete'):
+        article.key.delete()
+        return redirect('/')
     if form.is_valid():
-        if form.cleaned_data['delete']:
-            article.key.delete()
-        else:
-            article.title = form.cleaned_data['title']
-            article.content = form.cleaned_data['content']
-            article.last_update = datetime.now()
-            article.put()
+        article.title = form.cleaned_data['title']
+        article.content = form.cleaned_data['content']
+        article.last_update = datetime.now()
+        article.put()
         return redirect('/')
     return render(request, 'article.html', {'form': form})
 
