c6b1be7b6acd6298b484e3b0552f444b0cbdb5ff
konradko <konrad.korzel@gmail.com>
2013-12-05 00:02:12 +0000

    Add rows attr for content text area

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ class ArticleForm(forms.Form):
         error_messages={'required': "Title can't be empty"},
         widget=forms.TextInput(attrs={
             'placeholder': 'Enter title',
-             'class': "pure-input-1-2",
+            'class': "pure-input-1-2",
         })
     )
 
@@ -16,13 +16,14 @@ class ArticleForm(forms.Form):
         error_messages={'required': "Content can't be empty"},
         widget=forms.Textarea(attrs={
             'placeholder': 'Enter article',
+            'rows': 15,
             'class': "pure-input-1-2",
         })
     )
 
     def clean_content(self):
         data = self.cleaned_data['content']
-        if "[HTML_REMOVED]" in markdown(data , safe_mode=True):
+        if "[HTML_REMOVED]" in markdown(data, safe_mode=True):
             raise forms.ValidationError("You can't have HTML in content, use markdown syntax instead")
         return data
 
