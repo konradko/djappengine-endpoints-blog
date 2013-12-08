900ebd18986c1241ec7aeac25e6daab49962ef07
konradko <konrad.korzel@gmail.com>
2013-12-08 23:21:29 +0000

    Fix style, add allowed_client_ids, move check_if_exists method, remove user_required attr

--- blog/api.py
+++ blog/api.py
@@ -3,43 +3,48 @@ from protorpc import remote, message_types
 
 from blog.models import Article
 
-def check_if_exists(article):
-    if not article.from_datastore:
-        raise endpoints.NotFoundException('Article not found.')
+CLIENT_ID = 'YOUR-CLIENT-ID'
 
-@endpoints.api(name='article_api', version='v1', description='API for articles')
+@endpoints.api(
+    name='article_api',
+    version='v1',
+    description='API for articles',
+    allowed_client_ids=[CLIENT_ID, endpoints.API_EXPLORER_CLIENT_ID]
+)
 class ArticleApi(remote.Service):
 
+    def check_if_exists(self, article):
+        if not article.from_datastore:
+            raise endpoints.NotFoundException('Article not found.')
+
     @Article.method(
-        user_required=True,
         path='add',
-        http_method='POST', name='article.insert')
-    def ArticleInsert(self, article):
+        http_method='POST',
+        name='article.insert')
+    def add_article(self, article):
         article.put()
         return article
 
     @Article.method(
-        user_required=True,
         request_fields=('id', 'title', 'content'),
         path='patch/{id}',
         http_method='PUT',
         name='article.patch'
     )
-    def ArticleUpdate(self, article):
-        check_if_exists(article)
+    def update_article(self, article):
+        self.check_if_exists(article)
         article.put()
         return article
 
     @Article.method(
-        user_required=True,
         request_fields=('id',),
         path='delete/{id}',
         response_message=message_types.VoidMessage,
         http_method='DELETE',
         name='article.delete'
     )
-    def ArticleDelete(self, article):
-        check_if_exists(article)
+    def delete_article(self, article):
+        self.check_if_exists(article)
         article.key.delete()
         return message_types.VoidMessage()
 
@@ -49,15 +54,15 @@ class ArticleApi(remote.Service):
         http_method='GET',
         name='article.get'
     )
-    def ArticleGet(self, article):
-        check_if_exists(article)
+    def get_article(self, article):
+        self.check_if_exists(article)
         return article
 
     @Article.query_method(
         path='articles',
         name='article.list'
     )
-    def ArticleList(self, query):
+    def list_articles(self, query):
         return query
 
 application = endpoints.api_server([ArticleApi], restricted=False)
